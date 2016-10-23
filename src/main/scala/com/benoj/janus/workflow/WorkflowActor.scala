88ad79e2630c7d7a9e5f92f565b65e601c23835e
benoj <ben@pattr.net>
2016-10-23 12:22:24 +0100

    Workflow uses state pattern

--- src/main/scala/com/benoj/janus/workflow/WorkflowActor.scala
+++ src/main/scala/com/benoj/janus/workflow/WorkflowActor.scala
@@ -1,61 +1,95 @@
 package com.benoj.janus.workflow
 
 import akka.actor.{Actor, ActorLogging}
+import cats.data.Xor
+import com.benoj.janus.behavior.Created.NotFound
 import com.benoj.janus.workflow.WorkflowActor.Messages._
-import com.benoj.janus.workflow.WorkflowActor.WorkflowStage
-
-import scala.collection.mutable
+import com.benoj.janus.workflow.WorkflowActor.{WorkflowStage, WorkflowState}
 
 object WorkflowActor {
-  case class WorkflowStage(name: String, workUnits: mutable.Buffer[String] = mutable.Buffer.empty)
 
   object Messages {
+
     case class AddWorkUnit(id: String)
+
     case class DeleteWorkUnit(id: String)
+
     case class ProgressUnit(id: String)
+
     case class RegressUnit(id: String)
+
   }
+
+  case class WorkflowStage(name: String, workUnits: Set[String] = Set.empty) {
+    def add(workUnit: String) = copy(name, workUnits + workUnit)
+
+    def remove(workUnit: String) = copy(name, workUnits - workUnit)
+  }
+
+  case class WorkflowState(workflowStages: Seq[WorkflowStage]) {
+    def add(workUnit: String): WorkflowState = copy(workflowStages.tail.+:(workflowStages.head.add(workUnit)))
+
+    def remove(workUnit: String): Xor[NotFound.type, WorkflowState] = Xor.fromOption(workflowStages
+      .find(_.workUnits.contains(workUnit)).map { stage =>
+      workflowStages.splitAt(workflowStages.indexOf(stage)) match {
+        case (before, current :: after) => copy(before ++ after.+:(current.remove(workUnit)))
+        case _ => this
+      }
+    }, NotFound)
+
+    def progress(workUnit: String): Xor[NotFound.type, WorkflowState] = Xor.fromOption(workflowStages
+      .find(_.workUnits.contains(workUnit)).map { stage =>
+      workflowStages.splitAt(workflowStages.indexOf(stage)) match {
+        case (before, current :: (next :: after)) =>
+          copy(before.:+(current.remove(workUnit)) ++ after.+:(next.add(workUnit)))
+        case _ => this
+      }
+    }, NotFound)
+
+    def regress(workUnit: String): Xor[NotFound.type, WorkflowState] = Xor.fromOption(workflowStages
+      .find(_.workUnits.contains(workUnit)).map { stage =>
+      workflowStages.splitAt(workflowStages.indexOf(stage)) match {
+        case (start@_ :: previous :: Nil, current :: after) =>
+          copy((start :+ previous.add(workUnit)) ++ after.+:(current.remove(workUnit)))
+        case _ => this
+      }
+    }, NotFound)
+
+  }
+
 }
 
 class WorkflowActor(initialStages: Seq[WorkflowStage]) extends Actor with ActorLogging {
 
   log.info("Created Workflow")
-  private val stages: mutable.Buffer[WorkflowStage] = initialStages.toBuffer
+  private var state = WorkflowState(initialStages)
 
   override def receive: Receive = {
     case AddWorkUnit(id) =>
       log.info(s"Adding $id to workflow")
-      stages.head.workUnits.append(id)
-    case DeleteWorkUnit(id) => stages.find(_.workUnits.contains(id)) match {
-      case Some(stage) =>
-        log.info(s"Removing $id")
-        stage.workUnits.remove(stages.indexOf(stage))
-      case None =>
-    }
+      state = state.add(id)
+    case DeleteWorkUnit(id) =>
+      state.remove(id) match {
+        case Xor.Left(_) => log.error("Unknown Id")
+        case Xor.Right(s) =>
+          log.info(s"Removing $id")
+          state = s
+      }
     case ProgressUnit(id) =>
-      log.info(s"Progressing $id")
-      stages.find(_.workUnits.contains(id)) match {
-        case None =>
-        case Some(stage) =>
-          val currentStage =  stages.indexOf(stage)
-          stages.length match {
-            case i if i > currentStage =>
-              stages(currentStage + 1).workUnits.append(id)
-              stage.workUnits.remove(stage.workUnits.indexOf(id))
-              sender() ! "OK"
-            case _ =>
-          }
+      state.progress(id) match {
+        case Xor.Left(_) => log.error("Unknown Id")
+        case Xor.Right(s) =>
+          log.info(s"Progressing $id")
+          state = s
+          sender() ! "OK"
       }
     case RegressUnit(id) =>
-      log.info(s"Regressing $id")
-      stages.find(_.workUnits.contains(id)) match {
-        case None =>
-        case Some(stage) =>
-          val currentStage =  stages.indexOf(stage)
-          if(currentStage > 0) {
-            stages(currentStage - 1).workUnits.append(id)
-            stages(currentStage).workUnits.remove(currentStage)
-          }
+      state.regress(id) match {
+        case Xor.Left(_) => log.error("Unknown Id")
+        case Xor.Right(s) =>
+          log.info(s"Regressing $id")
+          state = s
+          sender() ! "OK"
       }
   }
 }
